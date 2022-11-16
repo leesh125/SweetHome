88c4ecc3669f247b4727aed41098e7134f8a29a1
LeeYunHwan <woooia1@gmail.com>
2022-11-16 17:21:41 +0900

    create Map Test

--- FrontEnd/whereismyhome/src/router/index.js
+++ FrontEnd/whereismyhome/src/router/index.js
@@ -13,10 +13,12 @@ const routes = [
   {
     path: '/house',
     name: 'house',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/HouseView.vue')
+  },
+  {
+    path: '/map/test',
+    name: 'mapTest',
+    component: () => import(/* webpackChunkName: "about" */ '../views/MapTestView.vue')
   }
 ]
 
